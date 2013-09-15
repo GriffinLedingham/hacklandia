ae5d336d07afc0c5eea766328a387c6ae996dcf8
Daniel Savage <dansvg@gmail.com>
2013-09-14 23:22:26 -0700

    Score increments on each minigame

--- PurpleCorgi/PurpleCorgi/PurpleCorgi/Game1.cs
+++ PurpleCorgi/PurpleCorgi/PurpleCorgi/Game1.cs
@@ -44,6 +44,8 @@ namespace PurpleCorgi
 
         private MetaGameState gameState = MetaGameState.Init;
 
+        public static int score;
+
         // running logic
         private float addMiniGameTimer;
         private const float addSecondMiniGameDuration = 3000f;
@@ -136,7 +138,7 @@ namespace PurpleCorgi
             g1.game = new PaddleMiniGame(GraphicsDevice);
             g1.canvas = new RenderTarget2D(GraphicsDevice, GameConstants.MiniGameCanvasWidth, GameConstants.MiniGameCanvasHeight);
 
-            g2.game = new SpaceGame(GraphicsDevice);
+            g2.game = new PlatformerGame(GraphicsDevice);
             g2.canvas = new RenderTarget2D(GraphicsDevice, GameConstants.MiniGameCanvasWidth, GameConstants.MiniGameCanvasHeight);
 
             g3.game = new TestMiniGame(GraphicsDevice);
@@ -227,6 +229,7 @@ namespace PurpleCorgi
                 miniGames = new List<MiniGameContext>();
                 addMiniGameTimer = 0;
                 gameState = MetaGameState.Running;
+                score = 0;
             }
         }
 
@@ -237,7 +240,6 @@ namespace PurpleCorgi
             addMiniGameTimer += gameTime.ElapsedGameTime.Milliseconds;
             if (miniGames.Count == 0 && addMiniGameTimer > 0)
             {
-                
                 miniGames.Add(g1);
             }
             else if (miniGames.Count == 1 && addMiniGameTimer > addSecondMiniGameDuration && Lobby.Difficulty > 1)
@@ -247,10 +249,6 @@ namespace PurpleCorgi
             else if (miniGames.Count == 2 && addMiniGameTimer > addThirdMiniGameDuration && Lobby.Difficulty > 2)
             {
                  miniGames.Add(g3);
-            }
-            else if (miniGames.Count == 3 && addMiniGameTimer > addFourthMiniGameDuration)
-            {
-                miniGames.Add(g4);
             }
 
             for (int i = 0; i < miniGames.Count; i++)
@@ -262,6 +260,8 @@ namespace PurpleCorgi
             {
                 if (me.game.GetState() == MiniGameState.Win)
                 {
+                    score += 100;
+
                     me.game = PureRandomMiniGame();
                 }
                 else if (me.game.GetState() == MiniGameState.Lose)
@@ -331,9 +331,9 @@ namespace PurpleCorgi
                 spriteBatch.Draw(Game1.WhitePixel, Lobby.Box1, Color.Red);
                 spriteBatch.DrawString(Game1.SegoeUIMono24, "Easy\nOne Game", new Vector2(Lobby.Box1.X, Lobby.Box1.Y) + new Vector2(8), Color.White);
                 spriteBatch.Draw(Game1.WhitePixel, Lobby.Box2, Color.Blue);
-                spriteBatch.DrawString(Game1.SegoeUIMono24, "Easy\nTwo Games", new Vector2(Lobby.Box2.X, Lobby.Box2.Y) + new Vector2(8), Color.White);
+                spriteBatch.DrawString(Game1.SegoeUIMono24, "Medium\nTwo Games", new Vector2(Lobby.Box2.X, Lobby.Box2.Y) + new Vector2(8), Color.White);
                 spriteBatch.Draw(Game1.WhitePixel, Lobby.Box3, Color.Green);
-                spriteBatch.DrawString(Game1.SegoeUIMono24, "Easy\nThree Games", new Vector2(Lobby.Box3.X, Lobby.Box3.Y) + new Vector2(8), Color.White);
+                spriteBatch.DrawString(Game1.SegoeUIMono24, "Hard\nThree Games", new Vector2(Lobby.Box3.X, Lobby.Box3.Y) + new Vector2(8), Color.White);
 
                 spriteBatch.Draw(Game1.WhitePixel, Lobby.Pointer, new Color(1, 1, 1, 0.5f));
                 
@@ -346,7 +346,7 @@ namespace PurpleCorgi
 
                 spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied);
 
-                spriteBatch.DrawString(SegoeUIMono24, "Align yourself into the center of the floor in front of the Kinect", new Vector2(75), Color.White);
+                spriteBatch.DrawString(SegoeUIMono24, "Align yourself in front of the Kinect", new Vector2(75), Color.White);
 
                 spriteBatch.Draw(Game1.WhitePixel, Lobby.Landing, Color.Red);
                 spriteBatch.Draw(Game1.WhitePixel, Lobby.User, new Color(1, 1, 1, 0.5f));
@@ -374,29 +374,33 @@ namespace PurpleCorgi
             {
                 // render mini games to screen
                 GraphicsDevice.SetRenderTarget(null);
-                GraphicsDevice.Clear(Color.Salmon);
+                GraphicsDevice.Clear(Color.Black);
                 spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, null, null, null, null, Matrix.Identity);
                 for (int i = 0; i < miniGames.Count; i++)
                 {
                     spriteBatch.Draw(miniGames[i].canvas, new Vector2(GameConstants.MiniGameCanvasWidth * (i % 2), GameConstants.MiniGameCanvasHeight * (i / 2)), Color.White);
                 }
+
+                spriteBatch.DrawString(SegoeUIMono24, "Score: " + score, new Vector2(640, 360) + new Vector2(16), Color.White);
                 spriteBatch.End();
             }
             else if (gameState == MetaGameState.PlayerLose)
             {
                 // render mini games to screen
                 GraphicsDevice.SetRenderTarget(null);
-                GraphicsDevice.Clear(Color.Salmon);
+                GraphicsDevice.Clear(Color.Black);
                 spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, null, null, null, BlackAndWhite, Matrix.Identity);
                 for (int i = 0; i < miniGames.Count; i++)
                 {
                     spriteBatch.Draw(miniGames[i].canvas, new Vector2(GameConstants.MiniGameCanvasWidth * (i % 2), GameConstants.MiniGameCanvasHeight * (i / 2)), Color.White);
                 }
+
+                spriteBatch.DrawString(SegoeUIMono24, "Score: " + score, new Vector2(640, 360) + new Vector2(16), Color.White);
                 spriteBatch.End();
 
                 spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, null, null, null, null, Matrix.Identity);
                 spriteBatch.Draw(Game1.whitePixel, new Rectangle((GameConstants.GameResolutionWidth / 2 - 200), (GameConstants.GameResolutionHeight / 2 - 100), 400, 200), Color.Blue);
-                spriteBatch.DrawString(SegoeUIMono24, "LOSE", (new Vector2(GameConstants.GameResolutionWidth, GameConstants.GameResolutionHeight) - SegoeUIMono24.MeasureString("LOSE")) / 2, Color.Cyan);
+                spriteBatch.DrawString(SegoeUIMono24, "You lost!\nScore: " + score, (new Vector2(GameConstants.GameResolutionWidth, GameConstants.GameResolutionHeight) - SegoeUIMono24.MeasureString("You lost!\nScore: " + score)) / 2, Color.Cyan);
                 spriteBatch.End();
             }
 
